44b83fb8f99257b12c8644fd44d81ee24f55b9b2
akremdakhli <akrem.dakhli1@esprit.tn>
2021-12-12 11:55:28 +0100

    second commit

---
+++ react-componet-master/src/component/adress.js
@@ -0,0 +1,20 @@
+import react from 'react';
+
+
+ function Adress (){
+
+        return(
+            <div className="content">
+            <h2>akrem.dakhli1@esprit.tn</h2>
+            <h3>..Tunis..tunisia..</h3>
+           <p>@copyright AKrem Dakhli
+               <a href="https://www.linkedin.com/in/akrem-dakhli-280784ba/">
+                   <span>Akrem Dakhli</span>
+               </a>
+           </p>
+            </div>
+        )
+}
+
+export default Adress
+
---
+++ react-componet-master/src/component/fullname.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+
+ function Fullname (){
+
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                <h1>Akrem <span>Dakhli </span></h1>
+                </div>
+        )
+}
+
+export default Fullname
